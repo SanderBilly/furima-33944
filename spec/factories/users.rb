fe618aaeaf26d72fe17b0cc9891ea6454ce7dc53
Sander <chikunotani223@gmail.com>
2021-01-11 18:19:19 +0900

    テスト用モデルの作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    first_name { "山田" }
+    last_name { "陸太郎" }
+    first_name_kana { "ヤマダ" }
+    last_name_kana { "リクタロウ" }
+    birthday {Faker::Date.between(from: '1930-01-01', to: '2016-12-31')}
+  end
+end
