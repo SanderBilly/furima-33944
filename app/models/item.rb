c8acefc2ff67a63ede6cc77be93dd2be1b169228
Sander <chikunotani223@gmail.com>
2021-01-12 16:43:30 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,5 +16,12 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  
   belongs_to :user
 end
