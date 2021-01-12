5a131b86db5c5999473ffd8026a7fbed76f0e71c
Sander <chikunotani223@gmail.com>
2021-01-12 16:29:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
   has_one_attached :image
   
   belongs_to :user
