2bc100dc1f32b541ba0df99ef7b4ea2009806302
Sander <chikunotani223@gmail.com>
2021-01-13 11:22:30 +0900

    テスト
    デプロイ後の挙動確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    if Item
+      @items = Item.includes(:user).order("created_at DESC")
+    end
   end
 end
