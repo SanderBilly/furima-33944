7908dd835e3ec201fb5c3b3ac4ca776bd4dfe3a2
Sander <chikunotani223@gmail.com>
2021-01-14 17:15:38 +0900

    バリデーションの設定

--- app/models/purchaser.rb
+++ app/models/purchaser.rb
@@ -1,4 +1,12 @@
 class Purchaser < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A[0-9]{9,11}\z/, message: "is invalid. Include half-width numbers." }
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
