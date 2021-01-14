bf85083ef65551c78c47a571f7766106807328e9
Sander <chikunotani223@gmail.com>
2021-01-14 13:52:35 +0900

    ordersコントローラーの生成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-  resources :items, only: [:new, :create, :show]
+  resources :items, only: [:new, :create, :show] do
+    resources :order, only: [:index]
+  end
 end
