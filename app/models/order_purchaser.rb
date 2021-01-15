a6ca3279e918f04302c8c292142b95cc259736f7
Sander <chikunotani223@gmail.com>
2021-01-15 16:49:08 +0900

    バリデーションの修正

--- app/models/order_purchaser.rb
+++ app/models/order_purchaser.rb
@@ -3,6 +3,8 @@ class OrderPurchaser
   attr_accessor :user_id, :item_id, :token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
