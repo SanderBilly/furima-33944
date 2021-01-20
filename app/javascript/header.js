3a93c264a616277403e6a5eaf19e727aa7020a44
Sander <chikunotani223@gmail.com>
2021-01-20 18:18:16 +0900

    商品詳細ページのpadding調整用JSの実装

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -5,5 +5,14 @@ function header(){
   main.style.paddingTop = (headerHeight + "px");
 }
 
+function showHeader(){
+  let headerHeight = document.querySelector(".top-page-header").clientHeight;
+  const show = document.querySelector(".item-show");
+  
+  show.style.paddingTop = (headerHeight + "px");
+}
+
 window.addEventListener("load", header);
 window.addEventListener("resize", header);
+window.addEventListener("load", showHeader);
+window.addEventListener("resize", showHeader);
