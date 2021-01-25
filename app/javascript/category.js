0ed18fc547a93239e246fe87de6d2e05b6733492
Sander <chikunotani223@gmail.com>
2021-01-26 00:38:10 +0900

    カテゴリメニューのJS設定

---
+++ app/javascript/category.js
@@ -0,0 +1,17 @@
+function category(){
+  const categoryAll = document.querySelector(".category");
+  const categoryTrigger = document.querySelector(".category-list");
+  const categoryList = document.querySelector(".category-parent");
+
+  categoryAll.addEventListener("mouseover", () => {
+      categoryTrigger.classList.add("open");
+      categoryList.classList.add("open");
+  });
+
+  categoryAll.addEventListener("mouseleave", () => {
+      categoryTrigger.classList.remove("open");
+      categoryList.classList.remove("open");
+  });
+}
+
+window.addEventListener("load", category);
