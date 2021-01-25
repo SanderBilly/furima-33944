aa45c57a00289f4327166dac9a85ca8c81d77f39
Sander <chikunotani223@gmail.com>
2021-01-26 00:41:35 +0900

    Update category.js

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -4,13 +4,23 @@ function category(){
   const categoryList = document.querySelector(".category-parent");
 
   categoryAll.addEventListener("mouseover", () => {
-      categoryTrigger.classList.add("open");
-      categoryList.classList.add("open");
+    categoryTrigger.classList.add("open");
+    categoryList.classList.add("open");
   });
 
   categoryAll.addEventListener("mouseleave", () => {
+    categoryTrigger.classList.remove("open");
+    categoryList.classList.remove("open");
+  });
+
+  categoryTrigger.addEventListener("click", () => {
+    if(categoryList.classList.contains("open")){
       categoryTrigger.classList.remove("open");
       categoryList.classList.remove("open");
+    } else {
+      categoryTrigger.classList.add("open");
+      categoryList.classList.add("open");
+    }
   });
 }
 
