048c409f97115d1c8d2143897f74fdda5b35f456
Sander <chikunotani223@gmail.com>
2021-01-26 01:10:39 +0900

    アクセス制限の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :search]
+  before_action :authenticate_user!, except: [:index, :show, :search, :category]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_category, only: [:index, :show, :search, :category]
   before_action :move_to_index, only: [:edit, :update, :destroy]
